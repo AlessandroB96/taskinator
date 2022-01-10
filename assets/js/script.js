991bdf2cd1a444f74bdf7a3e8f14686c2af6e229
Alessandro Belaj <abelaj7196@gmail.com>
2022-01-10 18:03:54 -0500

    sticky list items onto webpage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,17 @@
-var buttonEl = document.querySelector("#save-task");
-var tasksToDoEl = document.querySelector("#tasks-to-do");
+var formEl = document.querySelector("#task-form");
+var tasksToDoEl = document.querySelector("#tasks-to-do")
 
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {  
+
+
+  //event occuring is the form submission of createTaskHandler. form submission always defaults to reloading page, so this prevents the default loading so we can store the new task
+  event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
-  }
+    }
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
