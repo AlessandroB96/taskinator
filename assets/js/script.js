d7a31ddb9ad0a449057774ae56b991cad36dec21
Alessandro Belaj <abelaj7196@gmail.com>
2022-01-10 20:52:42 -0500

    Added user input validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,12 @@ var taskFormHandler = function(event) {
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-
+    if (!taskNameInput || !taskTypeInput) {
+      alert("You need to fill out the task form!");
+      return false;       //causes function to stop
+    }
+    
+    formEl.reset();
   //Package up the data as an object
   var taskDataObj = {
     name: taskNameInput,
